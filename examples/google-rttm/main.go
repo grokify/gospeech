0b2cf57281247f7057a1a6c8f11ccbddbf492261
John Wang <johncwang@gmail.com>
2019-09-17 08:57:31 -0700

    update example

--- examples/google-rttm/main.go
+++ examples/google-rttm/main.go
@@ -9,8 +9,6 @@ import (
 	iom "github.com/grokify/gotilla/io/ioutilmore"
 )
 
-//const speakerNamePrefix string = "S"
-
 /*
 
 https://godoc.org/google.golang.org/genproto/googleapis/cloud/speech/v1#LongRunningRecognizeResponse
@@ -20,21 +18,18 @@ https://godoc.org/google.golang.org/genproto/googleapis/cloud/speech/v1#LongRunn
 func main() {
 	file := "../mongodb-is-web-scale/web-scale_b2F-DItXtZs.mp3_tr_google_standard.json"
 
-	//file = "/Users/john.wang/jwdev/JGo/gopath/src/github.com/grokify/golang-samples/speech/captionasync/episode_1_mongo_db_is_web_scale_b2F-DItXtZs.mp3_transcript_gcs-standard.json"
-
 	res, err := diarization.ReadLongRunningRecognizeResponseFile(file)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmtutil.PrintJSON(res)
+
 	fmt.Printf("NUM_RESULTS [%v]\n", len(res.Results))
 
-	if 1 == 0 {
+	checkSpeakerTags = false
+	if checkSpeakerTags {
 		for i, resp := range res.Results {
 			for _, alt := range resp.Alternatives {
-				// Printf("[%v] %v\n", i, alt.Transcript)
 				for _, wrd := range alt.Words {
-					//fmtutil.PrintJSON(wrd)
 					if wrd.SpeakerTag > 0 {
 						fmt.Printf("%v ", i)
 					}
