db803f933fb74b7221f940f61c871281b0f93804
John Wang <johncwang@gmail.com>
2019-09-17 01:12:45 -0700

    add custom output file

--- data/mongodb-is-web-scale/convert.go
+++ data/mongodb-is-web-scale/convert.go
@@ -12,14 +12,19 @@ import (
 )
 
 var opts struct {
-	Input string `short:"i" long:"input" description:"transcription file" required:"true"`
-	Type  string `short:"t" long:"type" description:"file type" required:"true"`
+	Type   string `short:"t" long:"type" description:"file type" required:"true"`
+	Input  string `short:"i" long:"input" description:"transcription file" required:"true"`
+	Output string `short:"o" long:"output" description:"transcription file" required:"false"`
 }
 
 func main() {
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		log.Fatal(err)
+	}
+	opts.Output = strings.TrimSpace(opts.Output)
+	if len(opts.Output) == 0 {
+		opts.Output = opts.Input
 	}
 
 	switch strings.ToLower(strings.TrimSpace(opts.Type)) {
@@ -30,14 +35,15 @@ func main() {
 		}
 		//fmtutil.PrintJSON(txn)
 		rttm := diarization.TranscriptToRTTM(txn)
-		rttm.WriteFile(opts.Input+".rttm", 0644)
-		fmt.Printf("WROTE [%v]\n", opts.Input+".rttm")
-		err = iom.WriteFileJSON(opts.Input+".json", txn, 0644, "", "  ")
+		rttm.WriteFile(opts.Output+".rttm", 0644)
+		fmt.Printf("WROTE [%v]\n", opts.Output+".rttm")
+		err = iom.WriteFileJSON(opts.Output+".json", txn, 0644, "", "  ")
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("WROTE [%v]\n", opts.Input+".json")
+		fmt.Printf("WROTE [%v]\n", opts.Output+".json")
 	case "transcribeme":
+		panic("use NVivo PC instead of TranscribeMe native")
 		txn, err := diarization.ParseTranscribeMeFile(opts.Input)
 		if err != nil {
 			log.Fatal(err)
